8cf1538b9e38cdcaa2da49e8434331655d07b3d5
Developer MoleculaX <moleculax@gmail.com>
2025-08-17 22:01:57 -0300

    modif

--- test-JAVA/coleccionesEnJava/src/Main.java
+++ test-JAVA/coleccionesEnJava/src/Main.java
@@ -8,8 +8,8 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("COLECCIONES EJ JAVA!!!");
-//        sampleHashSet sampleHash = new sampleHashSet();
-//        sampleHash.hashS();
+        sampleHashSet sampleHash = new sampleHashSet();
+        sampleHash.hashS();
         sampleTreeSet treeS = new sampleTreeSet();
         treeS.treeSet();
 
